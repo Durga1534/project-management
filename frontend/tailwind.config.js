a1c9321e1354dc454dfadb14c2a947642b436ba4
DurgaPrasad <136675163+Durga1534@users.noreply.github.com>
2024-12-20 21:19:27 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,9 +3,20 @@ import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/daisyui/dist/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [formsPlugin, daisyui],
+  plugins: [
+    formsPlugin,
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+  darkMode: 'class',
 };
