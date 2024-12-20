205f89ce77a28588c5f1ed83bca1378e62a45155
DurgaPrasad <136675163+Durga1534@users.noreply.github.com>
2024-12-20 21:04:36 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,11 @@
+import formsPlugin from '@tailwindcss/forms';
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", 
-    "./node_modules/daisyui/dist/**/*.js",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
-  darkMode: 'class',
+  plugins: [formsPlugin, daisyui],
 };
